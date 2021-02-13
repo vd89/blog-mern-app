b9509681c89b8c5d1616b0d1ae921fd93a14f55b
vd89 <dixitvora@gmail.com>
2021-02-13 21:47:10 +0530

    BLOG_Chore: added the testing point

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 import express from 'express';
 import { Server } from 'http';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
 import serverConfig from './serverConfig.js';
 import dbController from './controller/dbController';
 
 const PORT = serverConfig.PORT;
+const ORIGIN = serverConfig.ORIGIN;
 const app = express();
 
 export const server = new Server(app);
 export const serverStarted = async () => {
+  app.use(morgan('combined'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(helmet());
+  app.use(cors({ ORIGIN }));
+
+  // Route
+  app.get('/ping', async (req, res, next) => {
+    try {
+      return res.status(200).json({ msg: 'Success', data: 'pong' });
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
+
+  // Connect to Db
   await dbController();
   server.listen(PORT, () => {
     console.log(`Server Is running at ${PORT}`);
--- server/serverConfig.js
+++ server/serverConfig.js
@@ -4,6 +4,7 @@ const serverConfig = {
   PORT: config.get('PORT') || process.env.PORT,
   MONGO_PASSWORD: config.get('MONGO_PASSWORD') || process.env.MONGO_PASSWORD,
   MONGO_USER: config.get('MONGO_USER') || process.env.MONGO_USER,
+  ORIGIN: "localhost:3000" 
 };
 
 export default serverConfig;
