d00134751a2a30d18710d0fde183b445233cb8c1
vd89 <dixitvora@gmail.com>
2021-02-13 19:31:02 +0530

    BLOG_Chore: Added the client and client script

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -1,9 +1,9 @@
 import config from 'config';
 
 const serverConfig = {
-  PORT: config.get('PORT') || '',
-  MONGO_PASSWORD: config.get('MONGO_PASSWORD'),
-  MONGO_USER: config.get('MONGO_USER'),
+  PORT: config.get('PORT') || process.env.PORT,
+  MONGO_PASSWORD: config.get('MONGO_PASSWORD') || process.env.MONGO_PASSWORD,
+  MONGO_USER: config.get('MONGO_USER') || process.env.MONGO_USER,
 };
 
 export default serverConfig;
